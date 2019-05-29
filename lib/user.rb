5af8a498d381ad5f86c740e642a32822d6e82d12
Scott <galejscott@gmail.com>
2019-05-29 16:20:04 +0100

    starting userinterface

--- lib/user.rb
+++ lib/user.rb
@@ -10,7 +10,7 @@ class User < ActiveRecord::Base
     hello = Song.find_by(title: "Hello")
 
     #Create
-    def create_playlist(name, user_id)
+    def create_playlist(name: name)
        Playlist.create(name: name, user_id: self.id)
     end
 
@@ -23,37 +23,41 @@ class User < ActiveRecord::Base
         self.playlists.map {|p| p.name}
     end
 
-    
+    def other_playlists
+        Playlist.all.select {|p| p.user_id != self.id}
+    end
 
 
-    def select_own_playlist(playlist_name)
-        self.playlists.find {|p| p.name == playlist_name}
+    def select_own_playlist(name: name)
+        self.playlists.find_by(name: name)
     end
 
 
 
     #Update
-    def add_song_to_playlist(playlist_name, song)
+    def add_song_to_playlist(name, song)
         # found_song = Song.find_by(title: song)
         # found_playlist = Playlist.find_by(name: playlist)
         # new_entry = Entry.create(playlist_id: found_playlist.id, song_id: found_song.id)
-        playlist = select_own_playlist(playlist_name)
+        playlist = select_own_playlist(name: name)
         Entry.create(playlist_id: playlist.id, song_id: song.id)
     end
 
     #Delete
-    def remove_song_from_playlist(playlist_name, song)
-        playlist = select_own_playlist(playlist_name)
+    def remove_song_from_playlist(name, song)
+        playlist = select_own_playlist(name: name)
         found_entry = Entry.find_by(playlist_id: playlist.id, song_id: song.id)
         found_entry.destroy
     end
 
-    
+    def delete_playlist(name: name)
+        playlist = select_own_playlist(name: name)
+        Entry.where(playlist_id: playlist.id).destroy_all
+        Playlist.where(id: playlist.id).destroy_all
+    end
     
 
-    def length_playlist
-        playlist.each {|song| song.length}.inject(:+)
-    end
+    
 
 
 end
--- lib/user_interface.rb
+++ lib/user_interface.rb
@@ -1,27 +1,67 @@
+$prompt = TTY::Prompt.new
 class UserInterface
 
-    def get_user
+    def welcome
         puts "Welcome to Playlist-me"
-
-        puts "Enter a username"
-        username = gets.chomp
+        
+        option = $prompt.select("Login or Register new user", ["Login", "Register new user"])
+            if option == "Login"
+                login = $prompt.collect do
+                    key(:username).ask("Username")
+                end
+            else option == "Register new user"
+                username = $prompt.collect do
+                    key(:username).ask("Username")
+                end
+            end
+        end
     end
 
-    def get_playlist
-        puts "Enter a playlist name"
-        Playlist.name = gets.chomp
+    def main_menu
+        option = $prompt.select("Main Menu", ["Create Playlist", "Select Existing Playlist", "Select Another Users Playlist", "Log out"])
+            if option == "Create Playlist"
+                #create playlist - enter name
+            elsif option == "Select Existing Playlist"
+                #show list of your own playlists
+            elsif option == "Select Another Users Playlist"
+                #show list of others playlists
+            else option == "Log out"
+                #back to welcome
+            end
+    end
+
+    def create
+        name = $prompt.collect do 
+            key(:name).ask("Please enter a Playlist name")
+            #create playlist
+        end
     end
 
     def add_song
-        puts "Lets add a song. Please pick an artist"
-        #drop down menu?
-        puts "Select a song"
-        #if song selected add to playlist. if not restart process
+        #select an artist
+        #select a song
+        #add? yes or no - no - back to select artist
     end
 
-    def see_other_playlist
-        
+    def remove_song
+        #remove song from playlist
     end
+
+    def delete_playlist
+        #delete playlist - back to mainmenu
+    end
+
+    def select_existing_playlist
+        #show list previously made playlists
+        #add songs
+        #remove songs
+        #delete playlist
+    end
+
+    def other_playlists
+        #show list of other users playlists
+    end
+
 end
 
     
